3468ab81fb53acbc23979f8eb723d004e5d0cdf4
anabastidas1 <107590971+anabastidas1@users.noreply.github.com>
2022-06-20 09:21:11 -0500

    Update process.php
    se corrigio

--- process.php
+++ process.php
@@ -17,7 +17,7 @@ if (!file_exists($nombre_fichero)) {
 if (filesize($nombre_fichero) == 0){
   $vacio = false;
 }else{
-  $file = fopen($nombre_fichero, "r") or exit("Error abriendo fichero!");
+  $file = fopen($nombre_fichero, "r") || exit("Error abriendo fichero!");
   $linea = fgets($file);
   $terminal =  explode("_", $linea);
   $mid = trim($terminal[0]);
@@ -31,14 +31,14 @@ if (filesize($nombre_fichero) == 0){
 $merchantId=$mid;//Homologación 7100040113
 $terminalId=$tid;//BP para OTT
 
-/*
+
 $merchantId="5000004001";//SUPERMAXI
 $terminalId="L0100402";
 
-*/
 
-/*$merchantId="1000000505";
-$terminalId="PD100406";*/
+
+$merchantId="1000000505";
+$terminalId="PD100406";
 
 
 
@@ -48,22 +48,20 @@ $terminalId="PD100406";*/
 $_SESSION['merchterm'] = $merchterm;
 
 
-/*
-**Low Risk - DATAFAST*/
-/*if($modalidad=='1'){
+
+**Low Risk - DATAFAST
+if($modalidad=='1'){
 	$_SESSION['entityId'] = "8a8294185a65bf5e015a6c8b89a10d8d";
 	$_SESSION['autorizador'] =  "OGE4Mjk0MTg1YTY1YmY1ZTAxNWE2YzhiMmY2OTBkOGJ8UmtqcHlOTkU4cw==";
-}else{*/
+}else{
 	$_SESSION['entityId'] = "8a8294175f113aad015f11652f2200a5";
 	$_SESSION['autorizador'] =  "OGE4Mjk0MTg1YTY1YmY1ZTAxNWE2YzhjNzI4YzBkOTV8YmZxR3F3UTMyWA==";
-//}
+}
 
-//$_SESSION['entityId']=$entity;
-//$_SESSION['autorizador']=$token;
-/*$_SESSION['userId']="8a8294185a65bf5e015a6c8b2f690d8b";
-$_SESSION['password']="RkjpyNNE8s";*/
-
-
+$_SESSION['entityId']=$entity;
+$_SESSION['autorizador']=$token;
+$_SESSION['userId']="8a8294185a65bf5e015a6c8b2f690d8b";
+$_SESSION['password']="RkjpyNNE8s";
 
 
 
@@ -71,8 +69,12 @@ $_SESSION['password']="RkjpyNNE8s";*/
 
 
 
-function request($items, $total,$iva,$totaTarifa12,$totalBase0,$email, $primer_nombre, $segundo_nombre, $apellido, $cedula, $trx,$ip_address, $finger,$merchterm,
-	$telefono, $direccion_cliente, $pais_cliente, $direccion_entrega, $pais_entrega) {
+
+
+function request($items, $total,$iva,$totaTarifa12,$totalBase0,$email, $primer_nombre)
+function request($segundo_nombre, $apellido, $cedula, $trx,$ip_address, $finger,$merchterm,
+	$telefono)
+function request($direccion_cliente, $pais_cliente, $direccion_entrega, $pais_entrega){
 	$finger = urlencode($finger);
 	$i = 0;
 	$url = "https://test.oppwa.com/v1/checkouts";
@@ -121,7 +123,7 @@ function request($items, $total,$iva,$totaTarifa12,$totalBase0,$email, $primer_n
 		'Authorization:Bearer '.$_SESSION['autorizador']));
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);// this should be set to true in production
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);// this should be set to true in production
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$responseData = curl_exec($ch);
 	if(curl_errno($ch)) {
@@ -133,11 +135,11 @@ function request($items, $total,$iva,$totaTarifa12,$totalBase0,$email, $primer_n
 
 $baseUrl = "https://pagostest.datafast.com.ec/df/payment.php";
 
-if(!is_float($totalBaseIva))
-	$totalBaseIva= number_format((float)$totalBaseIva, 2, '.', '');
+if(!is_float($totalBaseIva)){
+	$totalBaseIva= number_format((float)$totalBaseIva, 2, '.', '')};
 
-if(!is_float($totalBase0))
-	$totalBase0 = number_format((float)$totalBase0, 2, '.', '');
+if(!is_float($totalBase0)){
+	$totalBase0 = number_format((float)$totalBase0, 2, '.', '')};
 
 $iva =  $totalBaseIva * 0.12;
 $iva =  round($iva,2);
@@ -152,7 +154,7 @@ $json = json_decode($responseData, true);
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang='es'>
 <head>
 	<title></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -169,7 +171,7 @@ $json = json_decode($responseData, true);
 <div class="container">
 	<div class="row">
 		<div class="col-md-12">
-			<img src="../imagenes/logo-datafast.png">
+			<img src="../imagenes/logo-datafast.png",alt>
 		</div>
 		<div class="col-md-12">
 		<h1>Portal de compras</h1>
@@ -192,7 +194,7 @@ $json = json_decode($responseData, true);
 		</div>
 		<div class="row">
 		<div class="col-md-12 text-center">
-		<!-- <img src="../imagenes/marcas.png"> -->
+		<img src="../imagenes/marcas.png"> 
 		</div>
 	</div>
 	<p>Powered by <a href="http://www.datafast.com.ec/" target="_blank">Datafast</a></p>	
